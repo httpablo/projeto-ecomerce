0bd8a1e7d93184f7350fd1278a4342bb7e365810
Pablo Brito <pablobrith@gmail.com>
2024-09-30 19:29:23 -0300

    Models do app pedido

--- pedido/models.py
+++ pedido/models.py
@@ -15,3 +15,23 @@ class Pedido(models.Model):
       ('F', 'Finalizado'),
     )
   )
+
+  def __str__(self) -> str:
+    return f'Pedido N. {self.pk}'
+
+class ItemPedido(models.Model):
+  pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+  produto = models.CharField(max_length=255)
+  produto_id = models.PositiveBigIntegerField()
+  variacao = models.CharField(max_length=255)
+  variacao_id = models.PositiveBigIntegerField()
+  preco = models.FloatField()
+  quantidade = models.PositiveBigIntegerField()
+  imagem = models.CharField(max_length=2000)
+
+  def __str__(self) -> str:
+    return f'item do {self.pedido}'
+  
+  class Meta:
+    verbose_name = 'Item do pedido'    
+    verbose_name_plural = 'Itens do pedido'    
